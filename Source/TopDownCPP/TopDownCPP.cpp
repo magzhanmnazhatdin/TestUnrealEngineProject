995858773518458397a4b65faba8a84a287533bf
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2026-06-15 19:45:59 +0500

    Blueprint Project

--- Source/TopDownCPP/TopDownCPP.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TopDownCPP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownCPP, "TopDownCPP" );
-
-DEFINE_LOG_CATEGORY(LogTopDownCPP)
- 
