01f2bee09266ef63616706df1f06a24b02e75f15
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2026-06-16 20:08:00 +0500

    added rotation of enemies to player

--- Source/TopDown/Enemies/BaseEnemy.cpp
+++ Source/TopDown/Enemies/BaseEnemy.cpp
@@ -11,6 +11,15 @@ ABaseEnemy::ABaseEnemy()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationYaw = false;
+
+	if (GetCharacterMovement()) 
+	{
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	}
+
 }
 
 // Called when the game starts or when spawned
