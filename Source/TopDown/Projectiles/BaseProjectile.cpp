2d88d1de0f37ca4289fd9ed1369f6d77feb3b2e7
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2026-06-26 19:37:08 +0500

    added base projectile class

---
+++ Source/TopDown/Projectiles/BaseProjectile.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseProjectile.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+ABaseProjectile::ABaseProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->InitSphereRadius(15.0f);
+	SphereComp->SetCollisionProfileName(TEXT("Projectile"));
+	RootComponent = SphereComp;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->UpdatedComponent = SphereComp;
+	ProjectileMovement->InitialSpeed = 1200.0f;
+	ProjectileMovement->MaxSpeed = 1200.0f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 0.0f;
+	
+	InitialLifeSpan = 3.0f;
+}
+
+// Called when the game starts or when spawned
+void ABaseProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/TopDown/Projectiles/BaseProjectile.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class TOPDOWN_API ABaseProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile", meta = (AllowPrivateAccess = "true"))
+	USphereComponent* SphereComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
+public:	
+	FORCEINLINE USphereComponent* GetSphereComp() const { return SphereComp; }
+
+	FORCEINLINE UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+};
